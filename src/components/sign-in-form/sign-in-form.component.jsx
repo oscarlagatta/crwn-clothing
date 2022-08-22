9753705c3196e08eff881a3cbeda8d495dc47ec2
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-08-22 15:00:27 +0200

    finish auth page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -40,8 +40,19 @@ const SignInForm = () => {
         password
       );
       console.log(response);
+      resetFormFields();
     } catch (error) {
-      console.log(error);
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("Incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+        default:
+          console.log(error);
+          break;
+      }
     }
   };
 
@@ -74,7 +85,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button buttonType="google" onClick={signInWithGoogle}>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
             Google Sign In
           </Button>
         </div>

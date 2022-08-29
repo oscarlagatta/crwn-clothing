b020eb45caa9fb4b14a3e12c00db5b2afae52e42
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-08-29 14:10:39 +0200

    add products to cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,22 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
 import Button from "../button/button.component";
+
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>Go To Checkout</Button>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
+      <Button>Go To Checkout</Button>
     </div>
   );
 };

316788a729c9f98a5a9f55a4662cc0d82d603f74
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-09-01 12:36:42 +0200

    Styled-Component - Form Input Component

---
+++ src/components/form-input/form-input.styles.jsx
@@ -0,0 +1,62 @@
+/**
+ * css block is from styled component lets us encapsulate some css
+ * inside of a block that we can use a variable and inject into
+ * different components, we need to Convert css variable into regular
+ * jsx variables.
+ */
+
+import styled, { css } from "styled-components";
+
+const subColor = "grey";
+const mainColor = "black";
+
+const shrinkLabelStyles = css`
+  top: -14px;
+  font-size: 12px;
+  color: ${mainColor};
+`;
+
+/**we pick first things that are targeted later */
+export const FormInputLabel = styled.label`
+  color: ${subColor};
+  font-size: 16px;
+  font-weight: normal;
+  position: absolute;
+  pointer-events: none;
+  left: 5px;
+  top: 10px;
+  transition: 300ms ease all;
+
+  ${({ shrink }) => shrink && shrinkLabelStyles}
+`;
+
+export const Input = styled.input`
+  background: none;
+  background-color: white;
+  color: ${subColor};
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 100%;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid ${subColor};
+  margin: 25px 0;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus ~ ${FormInputLabel} {
+    ${shrinkLabelStyles};
+  }
+`;
+
+export const Group = styled.div`
+  position: relative;
+  margin: 45px 0;
+
+  input[type="password"] {
+    letter-spacing: 0.3em;
+  }
+`;

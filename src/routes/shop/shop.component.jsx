b21ad178e30aa25bc62fa8db8b05b57d1a42a48b
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-09-06 16:05:26 +0100

    Business Logic in category selector

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,30 +1,29 @@
-
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import { setCategoriesMap } from '../../store/categories/category.action';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import CategoriesPreivew from '../categories-preview/categories-preview.component';
-import Category from '../category/category.component';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import { setCategories } from "../../store/categories/category.action";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import CategoriesPreivew from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();
-  }, []);
+  }, [dispatch]);
 
-   return (
+  return (
     <Routes>
       <Route index element={<CategoriesPreivew />} />
       <Route path=":category" element={<Category />} />
     </Routes>
   );
-}
+};
 
 export default Shop;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -77,14 +77,15 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
+  // .reduce((acc, docSnapshot) => {
+  //   const { title, items } = docSnapshot.data();
+  //   acc[title.toLowerCase()] = items;
 
-    return acc;
-  }, {});
+  //   return acc;
+  // }, {});
 
-  return categoryMap;
+  // return categoryMap;
 };
 
 export const createUserDocumentFromAuth = async (

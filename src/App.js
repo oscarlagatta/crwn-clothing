23ed72ea1ff24b7f86367d8f758eec13cf188649
Oscar Lagatta <oscar.lagatta@mnscorp.net>
2022-09-14 10:21:31 +0100

    users saga work in progress

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { Route, Routes } from "react-router-dom";
-import {
-  getCurrentUser
-} from "./utils/firebase/firebase.utils";
 
 import Authentication from "./routes/authentication/authentication.component";
 import Home from "./routes/home/home.component";
@@ -13,14 +10,14 @@ import Navigation from "./routes/navigation/navigation.component";
 import Shop from "./routes/shop/shop.component";
 
 import Checkout from "./routes/checkout/checkout.component";
-
+import { checkuserSession } from "./store/user/user.action";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getCurrentUser().then((user) => console.log('user from firebase', user));
-  }, []);
+    dispatch(checkuserSession());
+  }, [dispatch]);
 
   return (
     <Routes>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -107,7 +107,7 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
-  return userDocRef;
+  return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
